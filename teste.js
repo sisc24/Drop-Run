468b5e48a2da69829514560b4090738aba57bb54
Sofia <sisc2904@gmail.com>
2025-04-30 08:17:32 +0100

    ajustes gerais + enigma 1

---
+++ teste.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "") {
+    x.innerHTML = "República Democrata do Congo";
+  } else {
+    x.innerHTML = "";
+  }
+}
