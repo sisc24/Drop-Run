e15fe369edd397156c774d4330e986457a32b396
Sofia <sisc2904@gmail.com>
2025-05-04 00:27:08 +0100

    +2 enigmas

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function abrirDocumentoAleatorio() {
   // Lista de arquivos HTML disponíveis
-  const documentos = ["enigmas.html", "Contactos.html", "Ajuda.html"];
+  const documentos = [
+    "codigo.html",
+    "Decaimento.html",
+    "direções.html",
+    "emissão-alfa.html",
+    "Vozes.html",
+    "resposta.html",
+    "vizinhos.html",
+  ];
 
   // Seleciona um documento aleatório
   const aleatorio = documentos[Math.floor(Math.random() * documentos.length)];
